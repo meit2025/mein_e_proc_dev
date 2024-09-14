5fbd9d6b089794ff089057c6aea1b3dc7f8c43cf
fajarsetiawan <fsetiawan295@gmail.com>
2024-09-14 09:04:03 +0700

    init commit

---
+++ resources/js/Pages/Dashboard/index.jsx
@@ -0,0 +1,12 @@
+export default function Index({ title }) {
+  const koplak = 'anajy';
+  const kontol = 'aasa';
+  return (
+    <>
+      <h1>My Super Blog</h1>
+      <h1>{title}</h1>
+      <>{koplak}</>
+      <hr />
+    </>
+  );
+}
