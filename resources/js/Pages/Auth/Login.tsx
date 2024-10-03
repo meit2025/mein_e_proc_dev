76fd21f155f3ef5432f3701c5ead98d0e9c5e271
fajarsetiawan <fsetiawan295@gmail.com>
2024-10-03 10:52:26 +0700

    fix: erro login

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -42,18 +42,18 @@ import { useAlert } from '../../contexts/AlertContext.jsx';
 import axiosInstance from '@/axiosInstance.js';
 
 export function LoginForm() {
-    const { errors } = usePage().props;
+  const { errors } = usePage().props;
 
-    const [loading, setLoading] = React.useState<boolean>(false);
+  const [loading, setLoading] = React.useState<boolean>(false);
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-        username: '',
-        password: '',
-        remember_me: false,
-        },
-    });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      password: '',
+      remember_me: false,
+    },
+  });
 
   const { showToast } = useAlert();
 
@@ -61,19 +61,19 @@ export function LoginForm() {
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
-        setLoading(true);
+      setLoading(true);
 
-        setTimeout(async() => {
-            setLoading(false);
+      setTimeout(async () => {
+        setLoading(false);
 
-            // Menggunakan axios untuk mengirim permintaan POST
-            const response = await axiosInstance.post('/login', values);
-            showToast(response.data.message, 'success');
-            window.location.href = '/';
-        },2000)
+        // Menggunakan axios untuk mengirim permintaan POST
+        const response = await axiosInstance.post('/login', values);
+        showToast(response.data.message, 'success');
+        window.location.href = '/';
+      }, 2000);
     } catch (error) {
-        const resultError = error as AxiosError;
-        console.log(resultError);
+      const resultError = error as AxiosError;
+      console.log(resultError);
     }
   }
 
@@ -140,29 +140,32 @@ export function LoginForm() {
         <div className='mt-4'>
           <Button className='w-full' variant={'blue'} type='submit' disabled={loading}>
             {loading ? (
-            <div className="flex justify-center items-center">
-              <svg
-                className="animate-spin h-5 w-5 mr-3 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24">
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-                ></path>
-              </svg>
-              Loading...
-            </div>
-          ) : ('Submit')}
+              <div className='flex justify-center items-center'>
+                <svg
+                  className='animate-spin h-5 w-5 mr-3 text-white'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                >
+                  <circle
+                    className='opacity-25'
+                    cx='12'
+                    cy='12'
+                    r='10'
+                    stroke='currentColor'
+                    strokeWidth='4'
+                  ></circle>
+                  <path
+                    className='opacity-75'
+                    fill='currentColor'
+                    d='M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z'
+                  ></path>
+                </svg>
+                Loading...
+              </div>
+            ) : (
+              'Submit'
+            )}
           </Button>
         </div>
       </form>
