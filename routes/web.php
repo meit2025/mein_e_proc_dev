e1621eec49798258ae3e1ee17e27620a6f7100a2
fajarsetiawan <fsetiawan295@gmail.com>
2024-10-07 22:16:15 +0700

    fix: notifikasi dan vendor

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,29 @@
 <?php
 
+use App\Events\GotMessage;
+use App\Events\NotifikasiUsers;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\NotifikasiController;
+use App\Jobs\SendNotification;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/login', [AuthController::class, 'login'])->name('login');
 // Route::post('/login', [AuthController::class, 'store'])->name('login.store');
 
+
+
 Route::group(['middleware' => 'auth'], function () {
+    Route::get('/test-notif', function () {
+        SendNotification::dispatch(Auth::user(),  'Test Notifikasi', 'gateway/api');
+        return response()->json([
+            'sukses' => 'sukses'
+        ]);
+    });
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/role', [DashboardController::class, 'roles'])->name('roles');
+    Route::get('/notifikasi', [NotifikasiController::class, 'index'])->name('index.notifikasi');
+    Route::get('/notifikasi/read', [NotifikasiController::class, 'read'])->name('read.notifikasi');
+    Route::get('/notifikasi/delete', [NotifikasiController::class, 'destory'])->name('destory.notifikasi');
 });
