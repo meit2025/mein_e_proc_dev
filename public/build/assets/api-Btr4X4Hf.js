4586b80cba650bf31660b5b523c374a549331c60
fajarsetiawan <fsetiawan295@gmail.com>
2024-10-15 09:39:36 +0700

    fix: bundel

---
+++ public/build/assets/api-Btr4X4Hf.js
@@ -0,0 +1,1 @@
+const E="/api/user",t=`${E}/list`,a=`${E}/create`,e=`${E}/update`,R=s=>`${E}/detail/${s}`,c=`${E}/delete`;export{a as C,c as D,e as E,t as G,R as a};
---
+++ public/build/assets/api-DWT5pdps.js
@@ -0,0 +1,1 @@
+const E="/api/secret",t=`${E}/list`,T=`${E}/create`,a=`${E}/update`,e=s=>`${E}/detail/${s}`,c=`${E}/delete`;export{T as C,c as D,a as E,t as G,e as a};
