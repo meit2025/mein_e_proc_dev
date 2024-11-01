984844bb9e0a2bfe30eab599055dc3ec0d4f2117
fajarsetiawan <fsetiawan295@gmail.com>
2024-11-01 09:06:25 +0700

    fix: data detail

---
+++ resources/js/Pages/Reimburse/components/ReimburseDetail.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const ReimburseDetail = () => {
+  return <p>Detail</p>;
+};
+
+export default ReimburseDetail;
---
+++ resources/js/components/detailPr/Po.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+interface PoProps {}
+
+const Po = (props: PoProps) => {
+  return <p>Po</p>;
+};
+
+export default Po;
