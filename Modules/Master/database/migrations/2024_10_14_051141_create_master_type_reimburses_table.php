819c03b57aa5f7be678cfb37f5f8ca08d4f8a158
fajarsetiawan <fsetiawan295@gmail.com>
2024-11-07 13:38:53 +0700

    fix: data drop if exsist

--- Modules/Master/database/migrations/2024_10_14_051141_create_master_type_reimburses_table.php
+++ Modules/Master/database/migrations/2024_10_14_051141_create_master_type_reimburses_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('master_type_reimburses');
         Schema::create('master_type_reimburses', function (Blueprint $table) {
             $table->id();
             $table->string('code')->unique();
