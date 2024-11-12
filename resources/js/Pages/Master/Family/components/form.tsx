e740a844da5c83647fbf132f9eb85255e850d292
alfinadisurya24 <alfinadisurya24@gmail.com>
2024-11-12 22:29:36 +0700

    feature: crud data family in detail user

--- resources/js/Pages/Master/Family/components/form.tsx
+++ resources/js/Pages/Master/Family/components/form.tsx
@@ -5,15 +5,18 @@ import {
   FormItem,
   FormMessage,
 } from '@/components/shacdn/form';
-import { useEffect, useState } from 'react';
+
 import { z } from 'zod';
+
 import { Button } from '@/components/shacdn/button';
+
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import '../css/index.scss';
+
 import { ScrollArea } from '@/components/shacdn/scroll-area';
-import { Separator } from '@/components/shacdn/separator';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/shacdn/tabs';
+
+import '../css/index.scss';
+import { CustomDatePicker } from '@/components/commons/CustomDatePicker';
 import {
   Select,
   SelectContent,
@@ -21,161 +24,154 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/shacdn/select';
-import { CustomDatePicker } from '@/components/commons/CustomDatePicker';
 import { Input } from '@/components/shacdn/input';
 import * as React from 'react';
-import axiosInstance from '@/axiosInstance';
-import axios, { AxiosError } from 'axios';
-import { CREATE_API_FAMILY, EDIT_FAMILY } from '@/endpoint/family/api';
 
-interface propsType {
+import axiosInstance from '@/axiosInstance';
+
+import { useAlert } from '@/contexts/AlertContext';
+import { AxiosError } from 'axios';
+import { FormType } from '@/lib/utils';
+
+export interface props {
   onSuccess?: (value: boolean) => void;
-  idUser?: string;
+  storeURL?: string;
+  editURL?: string;
+  updateURL?: string;
+  userId?: number;
+  type?: FormType;
 }
 
-export const FamilyHeaderForm = ({ onSuccess, idUser }: propsType) => {
+export default function FamilyHeaderForm({
+  onSuccess,
+  type = FormType.create,
+  storeURL,
+  editURL,
+  updateURL,
+  userId,
+}: props) {
   const formSchema = z.object({
-    total_family: z.string().min(1, 'Total Family is Required'),
-    families: z.array(
-      z.object({
-        id: z.string(),
-        status: z.string().min(1, 'Type Family is Must Choose'),
-        bod: z.date(),
-        name: z.string().min(1, 'Name Family is Required'),
-      }),
-    ),
+    userId: z.string(),
+    name: z.string().min(1, 'field name is required'),
+    status: z.string(),
+    bod: z.date(),
   });
 
-  const [activeTab, setActiveTab] = useState('family1');
-  const handleTabChange = (tabValue) => {
-    setActiveTab(tabValue);
-  };
-
-
-  const [totalFamily, setTotalFamily] = React.useState<string>('1');
-
-  const handleFormCountChange = (value: any) => {
-    setTotalFamily(value);
-    const currentForms = form.getValues('families');
-    const newForms = Array.from({ length: value }).map((_, index) => {
-      return (
-        currentForms[index] || {
-          id: '',
-          name: '',
-          status: '',
-          bod: new Date(),
-        }
-      );
-    });
-    form.setValue('families', newForms);
-    form.setValue('total_family', value.toString());
-  };
-
-  async function getDetailData() {
-    try {
-      const response = await axiosInstance.get(EDIT_FAMILY(idUser ?? ''));
-      const data = response.data.data;
-      if (data && data.length > 0) {
-        form.setValue('total_family', data.length.toString());
-        form.setValue(
-          'families',
-          data.map((val) => ({
-            id: val.id ?? '',
-            status: val.status ?? '',
-            name: val.name ?? '',
-            bod: val.bod ? new Date(val.bod) : new Date(),
-          })),
-        );
-      } else {
-        form.setValue('total_family', '1');
-        form.setValue('families', [
-          {
-            id: '',
-            name: '',
-            status: '',
-            bod: new Date(),
-          },
-        ]);
-      }
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        showToast(error.message, 'error');
-      } else {
-        showToast('Unknown error occurred', 'error');
-      }
-
-      form.setValue('total_family', '1');
-      form.setValue('families', [
-        {
-          id: '',
-          name: '',
-          status: '',
-          bod: new Date(),
-        },
-      ]);
-    }
-  }
-
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    try {
-      const updatedValues = {
-        ...values,
-        user: idUser
-      };
-      const response = await axiosInstance.post(CREATE_API_FAMILY, updatedValues);
-      onSuccess?.(true);
-      showToast(response.message, 'success');
-    } catch (e) {
-      onSuccess?.(false);
-      showToast(e.message, 'error');
-    }
+  const defaultValues = {
+    userId: userId,
+    name: '',
+    status: '',
+    bod: null,
   };
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      total_family: '1',
-      families: [
-        {
-          id: '',
-          status: '',
-          bod: new Date(),
-          name: '',
-        },
-      ],
-    },
+    defaultValues: defaultValues,
   });
 
-  useEffect(() => {
-    getDetailData();
-  }, [totalFamily]);
+  async function getDetailData() {
+    try {
+      const response = await axiosInstance.get(editURL);
+      const data = response.data.data;
+
+      form.reset({
+        userId: userId,
+        name: data.name,
+        status: data.status,
+        bod: new Date(data.bod),
+      });
+    } catch (e) {
+      const error = e as AxiosError;
+    }
+  }
+
+  const { showToast } = useAlert();
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      let response;
+      if (type === FormType.edit) {
+        response = await axiosInstance.put(updateURL ?? '', values);
+      } else {
+        response = await axiosInstance.post(storeURL ?? '', values);
+      }
+      onSuccess && onSuccess(true);
+      showToast(response?.data?.message, 'success');
+    } catch (e) {
+      onSuccess && onSuccess(false);
+      showToast(e.message, 'error');
+    }
+  };
+
+  React.useEffect(() => {
+    if (type === FormType.edit) {
+      getDetailData();
+    }
+  }, [type]);
 
   return (
-    <ScrollArea className='h-[600px] w-full '>
+    <ScrollArea className='h-[600px] w-full'>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-          <table className='text-xs mt-4 reimburse-form-table font-thin'>
+          <table className='text-xs mt-4 table font-thin'>
+            <FormField
+              control={form.control}
+              name='userId'
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type='hidden'
+                      {...field}
+                      value={field.value}
+                      onChange={(e) => field.onChange(e.target.value)}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <tr>
-              <td width={200}>Total Member</td>
+              <td width={200}>
+                Name <span className='text-red-500'>*</span>
+              </td>
               <td>
-                {' '}
                 <FormField
                   control={form.control}
-                  name='total_family'
+                  name='name'
                   render={({ field }) => (
                     <FormItem>
                       <FormControl>
-                        <Select
-                          onValueChange={(value) => handleFormCountChange(value)}
-                          value={field.value?.toString()}
-                        >
-                          <SelectTrigger className='w-[200px] py-2'>
-                            <SelectValue placeholder='-' />
+                        <Input
+                          type='text'
+                          placeholder='Insert name'
+                          {...field}
+                          value={field.value || ''}
+                          onChange={(e) => field.onChange(e.target.value)}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td width={200}>Status</td>
+              <td>
+                <FormField
+                  control={form.control}
+                  name='status'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Select onValueChange={(value) => field.onChange(value)} value={field.value}>
+                          <SelectTrigger className='w-[200px]'>
+                            <SelectValue placeholder='Child / Wife' />
                           </SelectTrigger>
                           <SelectContent>
-                            {Array.from({ length: 5 }, (_, index) => (
-                              <SelectItem value={(index + 1).toString()}> {index + 1} </SelectItem>
-                            ))}
+                            <SelectItem value='wife'>Wife</SelectItem>
+                            <SelectItem value='child'>Child</SelectItem>
                           </SelectContent>
                         </Select>
                       </FormControl>
@@ -185,135 +181,33 @@ export const FamilyHeaderForm = ({ onSuccess, idUser }: propsType) => {
                 />
               </td>
             </tr>
-          </table>
 
-          <Separator className='my-4' />
-
-          <Tabs value={activeTab} onValueChange={handleTabChange} className='w-full'>
-            <TabsList className={'flex items-center justify-start space-x-4'}>
-              {' '}
-              {/* Flexbox for horizontal layout */}
-              {Array.from({ length: form.watch('total_family') || 1 }).map((_, index) => (
-                <TabsTrigger key={index} value={`family${index + 1}`}>
-                  Member {index + 1}
-                </TabsTrigger>
-              ))}
-            </TabsList>
-
-            {Array.from({ length: form.watch('total_family') || 1 }).map((_, index) => (
-              <TabsContent key={index} value={`family${index + 1}`}>
+            <tr>
+              <td width={200}>Birth Of Data</td>
+              <td>
                 <FormField
                   control={form.control}
-                  name={`families.${index}.id`}
+                  name='bod'
                   render={({ field }) => (
                     <FormItem>
                       <FormControl>
-                        <Input
-                          type='text'
-                          className='sr-only'
-                          {...field}
-                          value={field.value.toString() || ''}
-                          onChange={(e) => field.onChange(e.target.value)}
+                        <CustomDatePicker
+                          initialDate={field.value}
+                          onDateChange={(date) => field.onChange(date)}
                         />
                       </FormControl>
+                      <FormMessage />
                     </FormItem>
                   )}
                 />
-                <div>
-                  <table className='text-xs mt-4 reimburse-form-detail font-thin'>
-                    <tr>
-                      <td width={200}>
-                        Name <span className='text-red-500'>*</span>
-                      </td>
-                      <td>
-                        <FormField
-                          control={form.control}
-                          name={`families.${index}.name`}
-                          render={({ field }) => (
-                            <FormItem>
-                              <FormControl>
-                                <Input
-                                  type='text'
-                                  placeholder='John Doe'
-                                  {...field}
-                                  value={field.value || ''}
-                                  onChange={(e) => field.onChange(e.target.value)}
-                                />
-                              </FormControl>
-                              <FormMessage />
-                            </FormItem>
-                          )}
-                        />
-                      </td>
-                    </tr>
-                    <tr>
-                      <td width={200}>Family Type</td>
-                      <td>
-                        <FormField
-                          control={form.control}
-                          name={`families.${index}.status`}
-                          render={({ field }) => (
-                            <FormItem>
-                              <FormControl>
-                                <Select onValueChange={(value) => field.onChange(value)} value={field.value}>
-                                  <SelectTrigger className='w-[200px]'>
-                                    <SelectValue placeholder='Child / Wife' />
-                                  </SelectTrigger>
-                                  <SelectContent>
-                                    <SelectItem value='wife'>Wife</SelectItem>
-                                    <SelectItem value='child'>Child</SelectItem>
-                                  </SelectContent>
-                                </Select>
-                              </FormControl>
-                              <FormMessage />
-                            </FormItem>
-                          )}
-                        />
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td width={200}>Birth of Date</td>
-                      <td>
-                        <FormField
-                          control={form.control}
-                          name={`families.${index}.bod`}
-                          render={({ field }) => (
-                            <FormItem>
-                              <FormControl>
-                                <CustomDatePicker
-                                  initialDate={
-                                    field.value instanceof Date
-                                      ? field.value
-                                      : new Date(field.value)
-                                  }
-                                  onDateChange={(date) => field.onChange(date)}
-                                />
-                              </FormControl>
-                              <FormMessage />
-                            </FormItem>
-                          )}
-                        />
-                      </td>
-                    </tr>
-                  </table>
-                </div>
-
-              </TabsContent>
-            ))}
-          </Tabs>
-          <Separator className='my-4' />
-          <div className='mt-4 flex justify-end'>
-            <Button type='submit' className='w-32'>
-              Update
-            </Button>
+              </td>
+            </tr>
+          </table>
+          <div className='flex justify-end items-center'>
+            <Button type='submit'>Save</Button>
           </div>
         </form>
       </Form>
     </ScrollArea>
   );
-};
-
-function showToast(arg0: string, arg1: string) {
-  throw new Error('Function not implemented.');
 }
