a2b978bcbbd2125547667d00f3d2365167166419
alfinadisurya24 <alfinadisurya24@gmail.com>
2024-11-19 08:10:28 +0700

    update: move detaill master reimburse quota to new page

--- resources/js/Pages/Master/MasterReimburseQuota/Index.tsx
+++ resources/js/Pages/Master/MasterReimburseQuota/Index.tsx
@@ -2,30 +2,22 @@ import MainLayout from '@/Pages/Layouts/MainLayout';
 import React, { ReactNode } from 'react';
 import DataGridComponent from '@/components/commons/DataGrid';
 
-import axiosInstance from '@/axiosInstance';
-import { AxiosError } from 'axios';
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/shacdn/dialog';
 import { columns } from './models/models';
-import { detailColumns } from './models/detailModels';
 import { Button } from '@/components/shacdn/button';
 import { PlusIcon } from 'lucide-react';
 import { CustomDialog } from '@/components/commons/CustomDialog';
-import { User } from './models/models';
 import ReimburseQuotaForm from './component/form'
 import { 
   LIST_API_REIMBURSE_QUOTA, 
   STORE_REIMBURSE_QUOTA, 
-  DETAIL_REIMBURSE_QUOTA, 
   EDIT_REIMBURSE_QUOTA, 
   UPDATE_REIMBURSE_QUOTA, 
   DESTROY_REIMBURSE_QUOTA } from '@/endpoint/reimburseQuota/api';
+import { DETAIL_REIMBURSE_QUOTA } from '@/endpoint/reimburseQuota/page';
 import { FormType } from '@/lib/utils';
 
 export const Index = () => {
   const [openForm, setOpenForm] = React.useState<boolean>(false);
-  const [openDetailModal, setOpenDetailModal] = React.useState<boolean>(false);
-  const [detailData, setDetailData] = React.useState<any>(null);
-  const [selectedId, setSelectedId] = React.useState<string | null>(null);
 
   const [formType, setFormType] = React.useState({
     type: FormType.create,
@@ -38,33 +30,6 @@ export const Index = () => {
       type: FormType.create,
       id: undefined,
     });
-  }
-
-  async function getDetailData($id: string) {
-    setSelectedId($id);
-    setOpenDetailModal(true);
-    fetchDetailData();
-  }
-
-  React.useEffect(() => {
-    if (selectedId && openDetailModal) {
-      fetchDetailData();
-    }
-
-  }, [selectedId, openDetailModal]);
-  
-  const fetchDetailData = async () => {
-    if (selectedId && openDetailModal) {
-      try {
-        const response = await axiosInstance.get(DETAIL_REIMBURSE_QUOTA(selectedId));
-        
-        setDetailData(response.data.data);
-      } catch (e) {
-        const error = e as AxiosError;
-        console.error(error);
-        setOpenDetailModal(false);
-      }
-    }
   }
 
   return (
@@ -100,74 +65,13 @@ export const Index = () => {
           });
           setOpenForm(true);
         }}
-        onDetail={(value) => {
-          getDetailData(value.toString())
-        }}
         url={{
           url: LIST_API_REIMBURSE_QUOTA,
+          detailUrl: DETAIL_REIMBURSE_QUOTA,
           deleteUrl: DESTROY_REIMBURSE_QUOTA,
         }}
         labelFilter='search'
       />
-
-      <Dialog 
-        open={openDetailModal} 
-        onOpenChange={(open) => {
-          setOpenDetailModal(open);
-          if (!open) {
-            setDetailData(null);
-            setSelectedId(null);
-          }
-        }}
-      >
-        <DialogContent className="max-w-5xl">
-          <DialogHeader>
-            <DialogTitle className="text-lg font-bold">
-              Preview Balance Reimburse Quota
-            </DialogTitle>
-          </DialogHeader>
-          <div className="py-4">
-            <table className='border-0 text-sm'>
-              <tr>
-                <td width="200">Reimburse Type</td>
-                <td>: {detailData?.type}</td>
-              </tr>
-              <tr>
-                <td width="200">Reimburse Period</td>
-                <td>: {detailData?.period}</td>
-              </tr>
-              <tr>
-                <td width="200">Employer Status</td>
-                <td>: {detailData?.employerStatus}</td>
-              </tr>
-              <tr>
-                <td width="200">Family Status</td>
-                <td>: {detailData?.familyStatus}</td>
-              </tr>
-            </table>
-            <table className='text-xs mt-4 table font-thin'>
-              <thead>
-                <tr>
-                  <th>Employer Name</th>
-                  <th>Family Name</th>
-                  <th>Balance</th>
-                  <th>Grade</th>
-                </tr>
-              </thead>
-              <tbody>
-                {detailData?.dataBalance && detailData?.dataBalance.map((value, index) => (
-                  <tr key={index}>
-                    <td>{value.employerName}</td>
-                    <td>{value.familyName}</td>
-                    <td>{value.plafon}</td>
-                    <td>{value.grade}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </DialogContent>
-      </Dialog>
     </>
   );
 };
--- resources/js/Pages/Master/MasterReimburseQuota/models/detailModels.tsx
+++ resources/js/Pages/Master/MasterReimburseQuota/models/detailModels.tsx
@@ -1,6 +1,6 @@
 import { GridColDef } from '@mui/x-data-grid';
 
-export const detailColumns: GridColDef[] = [
+export const columns: GridColDef[] = [
   { field: 'employerName', headerName: 'Employer Name', width: 200, filterable: true },
   { field: 'familyName', headerName: 'Family Name', width: 200, filterable: true },
   { field: 'plafon', headerName: 'Plafon', width: 200, filterable: true },
