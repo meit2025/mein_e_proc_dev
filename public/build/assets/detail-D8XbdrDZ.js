af69a7af88ff9ceb37f04d8fe6136a4ef942609b
alfianm <alfianmaulana191@yahoo.com>
2024-11-25 14:15:40 +0700

    build

---
+++ public/build/assets/detail-D8XbdrDZ.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-ySnIjE8D.js";import"./app-HlhieNAw.js";const n=r=>(r==null||r.detail,o.jsx(o.Fragment,{children:"role"}));export{n as DetailLayout};
---
+++ public/build/assets/print-lorrhOdR.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-ySnIjE8D.js";import"./app-HlhieNAw.js";const m=()=>r.jsx(r.Fragment,{});export{m as Print};
