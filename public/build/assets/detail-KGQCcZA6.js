2b6bc4c35a368f9b005461cccb515a64272fbc11
fajarsetiawan <fsetiawan295@gmail.com>
2024-12-02 14:48:51 +0700

    fix: approval delete

---
+++ public/build/assets/detail-KGQCcZA6.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-00v03VzZ.js";import"./app-HlhieNAw.js";const n=r=>(r==null||r.detail,o.jsx(o.Fragment,{children:"role"}));export{n as DetailLayout};
--- public/build/assets/print-fp3_ldZD.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DqrU2byG.js";import"./app-HlhieNAw.js";const m=()=>r.jsx(r.Fragment,{});export{m as Print};
