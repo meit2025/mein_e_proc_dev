56f85e4e48daa0b0be748fc4a2a25f4e49b363e8
fajarsetiawan <fsetiawan295@gmail.com>
2024-12-11 21:56:42 +0700

    fix: build portal

--- public/build/assets/StatusBolean-BjjJ5sbJ.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-B_GisuRo.js";const s=({status:e,name:a={active:"Active",InActive:"Inactive"}})=>t.jsx("span",{className:`badge badge-outline ${e?"badge-success":"badge-danger"}`,children:e?a.active:a.InActive});export{s as S};
