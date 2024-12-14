b703d9aff15d7dd65f71b1615a1a206a0977f45a
alfianm <alfianmaulana1411@gmail.com>
2024-12-14 15:00:16 +0700

    build

---
+++ public/build/assets/index-BQRrVA_2.js
@@ -0,0 +1,1 @@
+import{r as o,b as s}from"./app-B1AEVzT5.js";import{a as u}from"./index-DPFwwSoh.js";var i=s.useId||(()=>{}),c=0;function f(r){const[t,a]=o.useState(i());return u(()=>{a(e=>e??String(c++))},[r]),t?`radix-${t}`:""}export{f as u};
