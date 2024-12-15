68bc29588b2f742d6359e6f6e91344de4bd75e04
fajarsetiawan <fsetiawan295@gmail.com>
2024-12-15 08:59:43 +0700

    fix: build bug

---
+++ public/build/assets/index-pXDSnuaF.js
@@ -0,0 +1,1 @@
+import{r as o,$ as s}from"./app-D29rfeOn.js";import{a as u}from"./index-DQTnQjPn.js";var i=s.useId||(()=>{}),c=0;function f(r){const[t,a]=o.useState(i());return u(()=>{a(e=>e??String(c++))},[r]),t?`radix-${t}`:""}export{f as u};
