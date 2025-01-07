b0c7fcdd28682b4fef90f5e6c3ec2824e4fa96dc
alfinadisurya24 <alfinadisurya24@gmail.com>
2025-01-07 09:14:56 +0700

    update: wording pusat biaya to cost center

--- resources/js/Pages/Reimburse/components/ReimburseForm.tsx
+++ resources/js/Pages/Reimburse/components/ReimburseForm.tsx
@@ -114,7 +114,7 @@ export const ReimburseForm: React.FC<Props> = ({
   const formSchema = z.object({
     formCount: z.string().min(1, 'total form must be have value'),
     remark_group: z.string().optional(),
-    cost_center: z.string().min(1, 'pusat biaya required'),
+    cost_center: z.string().min(1, 'cost center required'),
     requester: z.string().min(1, 'requester required'),
     value: z.number().optional(),
     user_id: z.string().optional(),
