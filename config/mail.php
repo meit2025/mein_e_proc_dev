928aba98a599942c96e5e0d5411a0a369b3af7f3
fajarsetiawan <fsetiawan295@gmail.com>
2025-01-16 16:17:09 +0700

    fix: send mail

--- config/mail.php
+++ config/mail.php
@@ -50,6 +50,7 @@ return [
         ],
 
         'mein' => [
+            'transport' => 'smtp',
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
